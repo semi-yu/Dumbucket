5dcc8d89eb645e6da3fc1388b8e6bf978398724c
semi-yu <urban.manul.1919@gmail.com>
2025-11-23 15:20:55 +0900

    feat: Add endpoint `ping`
    API endpoint `/ping` returns simple string "pong". It can be used to
    check health.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+
+from src.connection import ping
 
 app = Flask(__name__)
+
+@app.route('/ping')
+def pong():
+    return ping()
+
+@app.route('/store', methods=['POST'])
+def store(): ...
+
+@app.route('/fetch', methods=['GET'])
+def fetch(): ...
 
 if __name__ == "__main__":
     app.run()
